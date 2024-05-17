67d2c879900f0fb3e211f55db30a20581a93cc2a
phillrog <phillrog@hotmail.com>
2024-05-17 19:57:33 -0300

    Implementação inicial do gateway  lista clientes

--- src/Client.Api/Controllers/ClientesController.cs
+++ src/Client.Api/Controllers/ClientesController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Client.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ClientesController : ControllerBase
     {
---
+++ src/Client.ApiGateway/Program.cs
@@ -0,0 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseOcelot();
+
+app.Run();
+
