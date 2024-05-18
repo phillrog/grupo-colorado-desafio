2a185ad85da5fcc0db34a61c0c3659e3d1125cad
phillrog <phillrog@hotmail.com>
2024-05-18 09:05:56 -0300

    Configuração pagina de erro para retornos 404

--- src/Client.Web/Program.cs
+++ src/Client.Web/Program.cs
@@ -28,17 +28,20 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseExceptionHandler("/Error");
+
+app.Use(async (ctx, next) =>
 {
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    await next();
+    if (ctx.Response.StatusCode == 404 && !ctx.Response.HasStarted)
+    {
+        ctx.Request.Path = "/Pages404";
+        await next();
+    }
+});
+
+app.UseHsts();
+
 
 using (var scope = app.Services.CreateScope())
 {
