319124230caffee7547a2b5be3b5ba95904086c3
phillrog <phillrog@hotmail.com>
2024-05-19 14:53:20 -0300

    Implementação de buscar na index e melhorias no layout

--- src/Client.Web/Areas/Clientes/Pages/Index.cshtml.cs
+++ src/Client.Web/Areas/Clientes/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Client.Web.Areas.Clientes.Services;
 using Client.Web.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Client.Web.Areas.Clientes.Pages
 {
@@ -16,11 +17,69 @@ namespace Client.Web.Areas.Clientes.Pages
         }
 
         public IList<Cliente> Cliente { get; set; } = default!;
+        
+        public string Search = "";
+        
+        public string column = "NomeFantasia";
+        public string orderBy = "desc";
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string? search, string? column, string? orderBy)
         {
             var result = await _clientesService.GetAllClients();
-            Cliente = result.Clientes.ToList();            
+            var query = new List<Cliente>(result.Clientes);
+            if (search != null)
+            {
+                this.Search = search;
+                query = query.Where(p => p.NomeFantasia.Contains(search) || p.RazaoSocial.Contains(search) || p.Cep.Contains(search)).ToList();
+            }
+            
+            string[] validColumn = { "RazaoSocial", "NomeFantasia", "Cep"};
+            string[] validOrderBy = { "desc", "asc" };
+
+            if (!validOrderBy.Contains(orderBy))
+            {
+                orderBy = "desc";
+            }
+            this.orderBy = orderBy;
+            this.column = column;
+
+            
+            if (column == "RazaoSocial")
+            {
+                if (orderBy == "asc")
+                {
+                    query = query.OrderBy(p => p.RazaoSocial).ToList();
+                }
+                else
+                {
+                    query = query.OrderByDescending(p => p.RazaoSocial).ToList();
+                }
+            }
+            else if (column == "NomeFantasia")
+            {
+                if (orderBy == "asc")
+                {
+                    query = query.OrderBy(p => p.NomeFantasia).ToList();
+                }
+                else
+                {
+                    query = query.OrderByDescending(p => p.NomeFantasia).ToList();
+                }
+            }
+            else if (column == "Cep")
+            {
+                if (orderBy == "asc")
+                {
+                    query = query.OrderBy(p => p.Cep).ToList();
+                }
+                else
+                {
+                    query = query.OrderByDescending(p => p.Cep).ToList();
+                }
+            }
+
+
+            Cliente = query;            
         }
     }
 }
